af5a9b8648f0dca4f9e353024d4ac0b916563951
Paul Potsides <paul@koble.ai>
2021-05-22 13:36:57 +0100

    Use hook to add class to the sidebar

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-document.body.className = `${document.body.className} player-role-${game.user.role}`
+Hooks.on('renderSidebarTab', function(){
+    const playerRoleClass = `player-role-${game.user.role}`
+    const sideBar = document.getElementById('sidebar');
+    if(sideBar && sideBar.className.indexOf(playerRoleClass) === -1) {
+        sideBar.className = `${sideBar.className} ${playerRoleClass}`;
+    }
+
+});
+
